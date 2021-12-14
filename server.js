cc4de9d7860b48e1e12716bff77fa7ca100b4d0d
marta <marta.cuccurullo00@gmail.com>
2021-12-14 14:29:44 +0100

    added socket

--- server.js
+++ server.js
@@ -4,10 +4,20 @@ let express = require("express");
 
 let app = express();
 
-let port = 3000;
+let port = 5501;
 
 let server = app.listen(port);
 
 console.log("Server is running on http://localhost:" + port);
 
 app.use(express.static("public"));
+
+let serverSocket = require("socket.io");
+
+let io = serverSocket(server);
+
+io.on("connection", newConnection);
+
+function newConnection(newSocket) {
+  console.log(newSocket.id);
+}
