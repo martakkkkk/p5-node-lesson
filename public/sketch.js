06b01e6dbdee3e4a2d13ebf1c93223208e1ed1b3
marta <marta.cuccurullo00@gmail.com>
2021-12-15 01:37:13 +0100

    final changes

--- public/sketch.js
+++ public/sketch.js
@@ -10,24 +10,24 @@ function newConnection() {
 function newBroadcast(data) {
   console.log(data);
   stroke("purple");
-  line(data.x - 66, data.y, data.x + 66, data.y);
-  line(data.x, data.y - 66, data.x, data.y + 66);
+  line(data.x - 33, data.y, data.x + 33, data.y);
+  line(data.x, data.y - 33, data.x, data.y + 33);
+}
+
+function preload() {
+  bg = loadImage("background.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background("black");
+  background(bg);
   strokeWeight(2);
 }
 
 function draw() {
-  stroke("white");
-  line(mouseX - 66, mouseY, mouseX + 66, mouseY);
-  line(mouseX, mouseY - 66, mouseX, mouseY + 66);
-
-  // stroke("black");
-  // fill("white");
-  // circle(mouseX, mouseY, 18);
+  stroke("lime");
+  line(mouseX - 33, mouseY, mouseX + 33, mouseY);
+  line(mouseX, mouseY - 33, mouseX, mouseY + 33);
 }
 
 function mouseMoved() {
